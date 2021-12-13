62b42bb610c2e65eaa7647f4e5cdf7ea625f7d31
Hburgos_Dev <hburgosl@estudiante.uniajc.edu.co>
2021-12-13 18:06:57 -0500

    cambio de ensayo

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,7 @@
-<h6>mano trae los culos locos
-    
+<h6>
+
+    mano trae los culos locos
+
 </h6>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
