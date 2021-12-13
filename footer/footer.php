58df8e353c989481b71daabc8ee5b68c5f9840d5
Hburgos_Dev <hburgosl@estudiante.uniajc.edu.co>
2021-12-13 15:27:14 -0500

    howen modificó código

---
+++ footer/footer.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css?v=<?php echo (rand()); ?>">
+</head>
+
+<body>
+    <footer>
+        <div class="info">
+            <h3>
+                Informacion <br>
+            </h3>
+            <h4>
+                Soplados Ultra Pet S.A <br>
+                Direccion sede principal calle 34 #78-54 <br>
+                Santiago de cali, Colombia | Todos los derechos reservados &copy; 2021
+            </h4>
+        </div>
+        <div class="redes">
+            <div class="container_tittle">
+                <h3>
+                    Redes sociales <br>
+                </h3>
+            </div>
+            <div class="container_icon">
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-instagram"></i>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
