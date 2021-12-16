82699c4256008611517dbea5eab1f8f62bed3fa8
AndyMuDz_Dev <andresrios508.ar@gmail.com>
2021-12-15 23:08:18 -0500

    modales

--- index.php
+++ index.php
@@ -13,29 +13,29 @@
  <div class="container-cards" >
      <div class="card">
          <img src="img/img1.jpg">
-         <h4>Naturaleza</h4>
+         <h4>MISIÓN</h4>
          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
-         <a href="#">Leer más</a>
+         <a href="#" class="abrir">Leer más</a>
      </div>
 
      <div class="card">
          <img src="img/img2.jpg">
-         <h4>Comida</h4>
+         <h4>VISIÓN</h4>
          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
-         <a href="#">Leer más</a>
+         <a href="#" class="abrir2">Leer más</a>
      </div>
 
      <div class="card">
          <img src="img/img3.jpg">
-         <h4>Tecnología</h4>
+         <h4>ACERCA DE NOSOTROS</h4>
          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, excepturi unde?</p>
-         <a href="#">Leer más</a>
+         <a href="#"  class="abrir3">Leer más</a>
      </div>
 
  </div>
 
  </div>
-
+<!-- modal1 -->
  <div class="container-modal">
      <div class="modal modal-close">
          <p class="close">X</p>
@@ -47,6 +47,32 @@
      </div>
  </div>
 
+<!-- modal 2 -->
+ <div class="container-modal2">
+     <div class="modal2 modal-close2">
+         <p class="close2">X</p>
+         <div class="modal-textos2">
+             <h1>Visión</h1>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia tempora ducimus placeat in laboriosam quibusdam, nobis nam, assumenda incidunt necessitatibus blanditiis? Officiis dolorem odio nemo consectetur excepturi doloribus quasi nobis.
+                 Nesciunt velit, vero facilis dignissimos perferendis optio dolores? Perspiciatis dolor qui delectus blanditiis quidem, provident quis modi accusamus! Ullam fugit ex earum nihil alias quos facere voluptatem facilis amet. Fuga.</p>
+         </div>
+     </div>
+ </div>
+
+
+ <!-- modal 3 -->
+ <div class="container-modal3">
+     <div class="modal3 modal-close3">
+         <p class="close3">X</p>
+         <div class="modal-textos3">
+             <h1>Acerca de nosotros</h1>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia tempora ducimus placeat in laboriosam quibusdam, nobis nam, assumenda incidunt necessitatibus blanditiis? Officiis dolorem odio nemo consectetur excepturi doloribus quasi nobis.
+                 Nesciunt velit, vero facilis dignissimos perferendis optio dolores? Perspiciatis dolor qui delectus blanditiis quidem, provident quis modi accusamus! Ullam fugit ex earum nihil alias quos facere voluptatem facilis amet. Fuga.</p>
+         </div>
+     </div>
+ </div>
+
+ 
  <?php
     include 'templates/footer.php';
     ?>
--- js/modal.js
+++ js/modal.js
@@ -1,19 +1,70 @@
+/*variables modal1 */
+
 let cerrar = document.querySelectorAll(".close")[0];
 let abrir = document.querySelectorAll(".abrir")[0];
 let modal = document.querySelectorAll(".modal")[0];
 let modalContainer = document.querySelectorAll(".container-modal")[0];
 
+/*variables modal2 */
+
+let cerrar2 = document.querySelectorAll(".close2")[0];
+let abrir2 = document.querySelectorAll(".abrir2")[0];
+let modal2 = document.querySelectorAll(".modal2")[0];
+let modalContainer2 = document.querySelectorAll(".container-modal2")[0];
+
+/*variables modal3 */
+
+let cerrar3 = document.querySelectorAll(".close3")[0];
+let abrir3 = document.querySelectorAll(".abrir3")[0];
+let modal3 = document.querySelectorAll(".modal3")[0];
+let modalContainer3 = document.querySelectorAll(".container-modal3")[0];
+
+/*modal 1 */
 abrir.addEventListener("click", function (e) {
-    e.preventDefault();
-    modalContainer.style.opacity = "1";
-    modalContainer.style.visibility = "visible";
-    modal.classList.toggle("modal-close");
+  e.preventDefault();
+  modalContainer.style.opacity = "1";
+  modalContainer.style.visibility = "visible";
+  modal.classList.toggle("modal-close");
 });
 
 cerrar.addEventListener("click", function () {
-    modal.classList.toggle("modal-close");
-    setTimeout(function () {
-        modalContainer.style.opacity = "0";
-        modalContainer.style.visibility = "hidden";
-    }, 900)
+  modal.classList.toggle("modal-close");
+  setTimeout(function () {
+    modalContainer.style.opacity = "0";
+    modalContainer.style.visibility = "hidden";
+  }, 900);
+});
+
+/*modal 2 */
+
+abrir2.addEventListener("click", function (f) {
+  f.preventDefault();
+  modalContainer2.style.opacity = "1";
+  modalContainer2.style.visibility = "visible";
+  modal2.classList.toggle("modal-close2");
+});
+
+cerrar2.addEventListener("click", function () {
+  modal2.classList.toggle("modal-close2");
+  setTimeout(function () {
+    modalContainer2.style.opacity = "0";
+    modalContainer2.style.visibility = "hidden";
+  }, 900);
+});
+
+/*modal 3 */
+
+abrir3.addEventListener("click", function (g) {
+  g.preventDefault();
+  modalContainer3.style.opacity = "1";
+  modalContainer3.style.visibility = "visible";
+  modal3.classList.toggle("modal-close3");
+});
+
+cerrar3.addEventListener("click", function () {
+  modal3.classList.toggle("modal-close3");
+  setTimeout(function () {
+    modalContainer3.style.opacity = "0";
+    modalContainer3.style.visibility = "hidden";
+  }, 900);
 });
